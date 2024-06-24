791a19fb41c75a1f8a0bdcdd934fc7f42f655694
Diana Dvoryak <dianadvoryak.w@gmail.com>
2024-06-24 16:43:18 +0200

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from src.shift_assignment.router import router as router_shift_assignment
+
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="App"
+)
+
+app.include_router(router_shift_assignment)
+
+@app.get("/")
+def unprotected_route():
+    return f"Hello, anonym"
+
---
+++ src/config.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
+SECRET_AUTH = os.environ.get("SECRET_AUTH")
+
+SMTP_HOST = os.environ.get("EMAIL_HOST")
+SMTP_USER = os.environ.get("EMAIL_HOST_USER")
+SMTP_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+SMTP_PORT = os.environ.get("EMAIL_PORT")
+
+DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
+DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
+DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+DB_USER_TEST = os.environ.get("DB_USER_TEST")
+DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
